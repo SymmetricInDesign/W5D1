df11523dff3dcf67ed8cb2d50071a6025a35e39f
Donnie Wombough <donniewomboughjunior@yahoo.com>
2021-05-17 16:03:17 -0400

    add string and array hashing methods

--- Hash Map & LRU Cache/lib/p01_int_set.rb
+++ Hash Map & LRU Cache/lib/p01_int_set.rb
@@ -61,7 +61,7 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_accessor :count
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -69,18 +69,29 @@ class ResizingIntSet
   end
 
   def insert(num)
+    return false if self.include?(num)
+    self[num] << num
+    self.count += 1
+    resize! if num_buckets < self.count
+    return num
   end
 
   def remove(num)
+    if self.include?(num)
+      self[num].delete(num)
+      @count -=1
+    end
   end
 
   def include?(num)
+    arr = self[num]
+    arr.include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets] # optional but useful; return the bucket corresponding to `num`
   end
 
   def num_buckets
@@ -88,6 +99,44 @@ class ResizingIntSet
   end
 
   def resize!
+    new_arr = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |el|
+        new_arr[el % (num_buckets * 2)] << el
+      end
+    end
+    @store = new_arr
   end
 end
 
+
+    # # if !self.include?(num)
+    # #   @count += 1
+    # #   if @count >= num_buckets
+    # #     old_store = @store
+    # #     @store = Array.new(num_buckets + 1) { Array.new } 
+    # #     @store.each do |bucket|
+    # #       bucket.each do |element|
+
+    # #       end
+    # #     end
+    # #   else
+
+
+    # #   end
+
+      
+      
+      
+    #   # @store.each do |bucket|
+    #   #   bucket.each_with_index do |el, idx|
+    #   #     to_move = el
+    #   #     bucket[idx] = nil
+
+    #   #   end
+    #   #   bucket.delete(nil)
+    #   # end
+    # else
+    #   return false
+    # end
+    # true
--- Hash Map & LRU Cache/lib/p02_hashing.rb
+++ Hash Map & LRU Cache/lib/p02_hashing.rb
@@ -4,11 +4,49 @@ end
 
 class Array
   def hash
+    return 2349847234 if self.length < 1 
+    hash_int = 1
+    self.each_with_index do |el, idx|
+      hash_int += el.to_s.length*27
+      if el.is_a?(String)
+        hash_int += 7453
+        if el.length >= 3
+          hash_int /= el[2].ord
+          hash_int += 8
+        else
+          hash_int *= 35
+          hash_int += 4
+        end
+      elsif el.is_a?(Array)
+        el.hash
+      elsif el.is_a?(Hash)
+        hash_int += el.keys.length
+        if el.keys.length >= 3
+          hash_int *= el.keys[2].to_s.length + 15 
+        end
+      else
+        hash_int += 234234
+        hash_int /= 3
+      end
+      hash_int *= idx^4/7
+    end
+    hash_int
   end
 end
 
 class String
   def hash
+    return 293842347627 if self.length == 0
+    hash_int = 7453 * (self.length + 456)
+    if self.length >= 3
+      hash_int /= self[2].ord
+      hash_int += 8
+    else
+      hash_int *= 35
+      hash_int += 4
+    end
+    self.each_char.with_index {|char, idx| hash_int += char.ord**7 + ((idx + 234)%15)**5 + 14*idx}
+    hash_int
   end
 end
 
@@ -19,3 +57,6 @@ class Hash
     0
   end
 end
+
+# p [3,"cat", "doggo", {:fred => 12, "fly" => 6554, 34576 => "rat"}].hash
+# p "sdhjkfsdfb".hash
